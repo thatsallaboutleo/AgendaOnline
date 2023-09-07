ac4f5feeb7c68d5aeccaa60500b8ada78cd9b20d
leona <leona@DESKTOP-DVNOFIA>
2023-09-07 09:16:04 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgendaOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
